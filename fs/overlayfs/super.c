72138b39f8099c4ac24e88eae5e8f9fc6c53a061
shencui520 <74359805+shencui520@users.noreply.github.com>
2026-08-10 22:38:29 +0800

    ovl: support encrypted F2FS as Docker overlay upper

--- fs/overlayfs/super.c
+++ fs/overlayfs/super.c
@@ -344,45 +344,52 @@ bug:
 	return dentry;
 }
 
-static int ovl_dentry_revalidate(struct dentry *dentry, unsigned int flags)
+static int ovl_revalidate_real(struct dentry *d, unsigned int flags,
+			       bool weak)
 {
-	struct ovl_entry *oe = dentry->d_fsdata;
-	unsigned int i;
 	int ret = 1;
 
-	for (i = 0; i < oe->numlower; i++) {
-		struct dentry *d = oe->lowerstack[i].dentry;
-
-		if (d->d_flags & DCACHE_OP_REVALIDATE) {
-			ret = d->d_op->d_revalidate(d, flags);
-			if (ret < 0)
-				return ret;
-			if (!ret) {
-				if (!(flags & LOOKUP_RCU))
-					d_invalidate(d);
-				return -ESTALE;
-			}
-		}
-	}
-	return 1;
-}
-
-static int ovl_dentry_weak_revalidate(struct dentry *dentry, unsigned int flags)
-{
-	struct ovl_entry *oe = dentry->d_fsdata;
-	unsigned int i;
-	int ret = 1;
-
-	for (i = 0; i < oe->numlower; i++) {
-		struct dentry *d = oe->lowerstack[i].dentry;
-
-		if (d->d_flags & DCACHE_OP_WEAK_REVALIDATE) {
+	if (weak) {
+		if (d->d_flags & DCACHE_OP_WEAK_REVALIDATE)
 			ret = d->d_op->d_weak_revalidate(d, flags);
-			if (ret <= 0)
-				break;
+	} else if (d->d_flags & DCACHE_OP_REVALIDATE) {
+		ret = d->d_op->d_revalidate(d, flags);
+		if (!ret) {
+			if (!(flags & LOOKUP_RCU))
+				d_invalidate(d);
+			ret = -ESTALE;
 		}
 	}
 	return ret;
+}
+
+static int ovl_dentry_revalidate_common(struct dentry *dentry,
+					unsigned int flags, bool weak)
+{
+	struct ovl_entry *oe = dentry->d_fsdata;
+	struct dentry *upper;
+	unsigned int i;
+	int ret = 1;
+
+	upper = ovl_dentry_upper(dentry);
+	if (upper)
+		ret = ovl_revalidate_real(upper, flags, weak);
+
+	for (i = 0; ret > 0 && i < oe->numlower; i++)
+		ret = ovl_revalidate_real(oe->lowerstack[i].dentry, flags, weak);
+
+	return ret;
+}
+
+static int ovl_dentry_revalidate(struct dentry *dentry, unsigned int flags)
+{
+	return ovl_dentry_revalidate_common(dentry, flags, false);
+}
+
+static int ovl_dentry_weak_revalidate(struct dentry *dentry,
+				      unsigned int flags)
+{
+	return ovl_dentry_revalidate_common(dentry, flags, true);
 }
 
 static const struct dentry_operations ovl_dentry_operations = {
@@ -931,13 +938,19 @@ static int ovl_mount_dir(const char *name, struct path *path)
 		ovl_unescape(tmp);
 		err = ovl_mount_dir_noesc(tmp, path);
 
-		if (!err)
-			if (ovl_dentry_remote(path->dentry)) {
-				pr_err("overlayfs: filesystem on '%s' not supported as upperdir\n",
-				       tmp);
-				path_put(path);
-				err = -EINVAL;
-			}
+		/*
+		 * Android's encrypted F2FS uses d_revalidate(), even though it is
+		 * a local filesystem.  Overlayfs must preserve that callback rather
+		 * than rejecting the upperdir as if it were a remote filesystem.
+		 * Keep the historical rejection for every other filesystem.
+		 */
+		if (!err && ovl_dentry_remote(path->dentry) &&
+		    strcmp(path->mnt->mnt_sb->s_type->name, "f2fs")) {
+			pr_err("overlayfs: filesystem on '%s' not supported as upperdir\n",
+			       tmp);
+			path_put(path);
+			err = -EINVAL;
+		}
 		kfree(tmp);
 	}
 	return err;
@@ -1183,6 +1196,9 @@ static int ovl_fill_super(struct super_block *sb, void *data, int silent)
 		err = ovl_mount_dir(ufs->config.upperdir, &upperpath);
 		if (err)
 			goto out_free_config;
+
+		if (ovl_dentry_remote(upperpath.dentry))
+			remote = true;
 
 		/* Upper fs should not be r/o */
 		if (upperpath.mnt->mnt_sb->s_flags & MS_RDONLY) {
